70cb0f62228c8785d81665f3d8aa344b6b5e6758
Rick Smoke <rsmoke@umich.edu>
2021-02-02 16:58:46 -0500

    add static pages and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/terms', to: 'static_pages#terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
